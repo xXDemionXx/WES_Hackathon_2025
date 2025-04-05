f3f6c1d5bed6fdafe5057dc2cbd4d11f0679c03c
Demion <demion.savrljuga.business@gmail.com>
2025-04-05 17:33:53 +0200

    gui.h updated with structs

--- components/gui/gui.h
+++ components/gui/gui.h
@@ -18,6 +18,70 @@ extern "C" {
 
 //---------------------------------- MACROS -----------------------------------
 
+#define GUI_SEND_QUEUE_LENGTH               5
+#define GUI_SEND_QUEUE_WAIT                 100
+
+/**
+ * @brief Structure that will be sent over the queue.
+ * 
+ */
+typedef enum{
+    GUI_MAIN_MESSAGE,
+    GUI_SIDE_MESSAGE
+}gui_send_message_t;
+
+typedef struct{
+    int button1;
+    int button2;
+}gui_send_data_1_t;
+
+typedef struct{
+    int x_axis;
+    int y_axis;
+}gui_send_data_2_t;
+
+typedef union{  // Depending on the enum we want to send different data
+    gui_send_data_1_t buttons_data;
+    gui_send_data_2_t joystick_data;
+}gui_send_message_data_t;
+
+typedef struct{
+    gui_send_message_t message_type;
+    gui_send_message_data_t data;
+}gui_send_queue_data_t;
+
+
+#define GUI_RECEIVE_QUEUE_LENGTH           5
+#define GUI_RECEIVE_QUEUE_WAIT             10
+
+/**
+ * @brief Structure that will be received over the queue.
+ * 
+ */
+typedef enum{
+    GUI_COMMAND1,
+    GUI_COMMAND2
+}gui_receive_message_t;
+
+typedef struct{
+    int button1;
+    int button2;
+}gui_receive_data_1_t;
+
+typedef struct{
+    int x_axis;
+    int y_axis;
+}gui_receive_data_2_t;
+
+typedef union{  // Depending on the enum we want to send different data
+    gui_receive_data_1_t command1;
+    gui_receive_data_2_t command2;
+}gui_receive_message_data_t;
+
+typedef struct{
+    gui_receive_message_t message_type;
+    gui_receive_message_data_t data;
+}gui_receive_queue_data_t;
 //-------------------------------- DATA TYPES ---------------------------------
 
 //---------------------- PUBLIC FUNCTION PROTOTYPES --------------------------
